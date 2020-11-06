2f5020aa97a7ba80c5afe078a97f0b3691ed3506
Angel Contreras <angel@transactility.com>
2020-11-05 19:52:25 -0600

    implments route with stinky if's

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,40 +2,57 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
 
+  def log(conv), do: IO.inspect conv
+
   def parse(request) do
-    # TODO: Parse the request string into a map
     [method, path, _] =
       request
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
 
-    %{method: method, path: path, response_body: ""}
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "wildthings", response_body: "Bears, Lion, Tigers"}
+    if conv.path == "/wildthings" do
+      %{conv | resp_body: "Bears, Lions, Tigers"}
+    else
+      %{conv | resp_body: "Teddy, Smokey, Paddington"}
+    end
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lion, Tigers
+    #{conv.resp_body}
     """
   end
 end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+Bears, Lion, Tigers
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
