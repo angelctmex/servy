c7484d95cc83d013ade357b29011e1dea7a86100
Angel Contreras <angel@transactility.com>
2020-11-09 15:22:43 -0600

    video: 15 - enums

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,6 +5,7 @@ defmodule Servy.Handler do
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
   alias  Servy.Conv
+  alias  Servy.BearController
 
   @doc "Transforms the request into a response."
    def handle(request) do
@@ -22,16 +23,16 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/bears"} = conv) do
-    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+    BearController.index(conv)
   end
 
   def route(%Conv{ method: "GET", path: "/bears/" <> id} = conv) do
-     %{conv | status: 200, resp_body: "Bear #{id}"}
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{ method: "POST", path: "/bears"} = conv) do
-    %{conv | status: 200,
-             resp_body: "Create a #{conv.params["type"]} bear names #{conv.params["name"]} "}
+    BearController.create(conv, conv.params)
   end
 
 
