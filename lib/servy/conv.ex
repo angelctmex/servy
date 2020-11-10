f12dcc2dbe713a957430520b208f5c9173e3fd6a
Angel Contreras <angel@transactility.com>
2020-11-09 21:44:54 -0600

    video: 18 - Testing

--- lib/servy/conv.ex
+++ lib/servy/conv.ex
@@ -7,7 +7,7 @@ defmodule Servy.Conv do
             headers: %{}
 
   def full_status(conv) do
-    #{conv.status} #{status_reason(conv.status)}
+   "#{conv.status} #{status_reason(conv.status)}"
   end
 
   defp status_reason(code) do
